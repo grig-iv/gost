3b85a4255579b5159b777f8d6853aae7b7667dbf
grig-iv <abstractgrig@gmail.com>
2024-10-06 21:03:33 +0300

    add flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "GOST flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        packages.default = pkgs.buildGoPackage {
+          name = "GOST";
+          src = ./.;
+          goPackagePath = "github.com/grig-iv/gost";
+        };
+      }
+    );
+}
